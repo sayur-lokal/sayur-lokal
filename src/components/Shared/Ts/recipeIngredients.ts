277c2f79ebacea8869b491ad43494b63193e7d6d
yvitamn <mnyovita516@gmail.com>
2025-04-25 00:18:04 +0700

    chore: move files to src/components/Shared/Ts/ folder

---
+++ src/components/Shared/Ts/recipeIngredients.ts
@@ -0,0 +1,32 @@
+
+export const sayurAsemIngredients = [
+    "Asam Jawa",
+    "Kacang Panjang",
+    "Jagung Manis",
+    "Labu Siam",
+    "Melinjo",
+    "Daun Salam",
+    "Bumbu Racik Sayur Asem",
+  ];
+
+  export const sayurSopIngredients = [
+    "Wortel",
+    "Kentang",
+    "Kol",
+    "Buncis",
+    "Daun Seledri",
+    "Daun Bawang",
+    "Bawang Putih",
+    "Merica Bubuk",
+    "Garam",
+    "Kaldu Bubuk",
+    "Air"
+  ];
+
+  export const optionalSopIngredients = [
+    "Bakso",
+    "Ayam Suwir",
+    "Sosis",
+    "Tomat"
+  ];
+  
