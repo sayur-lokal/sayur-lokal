68009d9d27d9d61dbcdab9da3b23b69f2509f797
nafiyah99 <nafiyah99@gmail.com>
2025-05-01 19:47:14 +0700

    fix: remove breadcrumbs and adjusting margin top

--- src/components/Auth/Signin/index.tsx
+++ src/components/Auth/Signin/index.tsx
@@ -1,12 +1,10 @@
-import Breadcrumb from "@/components/Common/Breadcrumb";
 import Link from "next/link";
 import React from "react";
 
 const Signin = () => {
   return (
     <>
-      <Breadcrumb title={"Signin"} pages={["Signin"]} />
-      <section className="overflow-hidden py-20 bg-gray-2">
+      <section className="overflow-hidden py-20 mt-30 bg-gray-2">
         <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0">
           <div className="max-w-[570px] w-full mx-auto rounded-xl bg-white shadow-1 p-4 sm:p-7.5 xl:p-11">
             <div className="text-center mb-11">
