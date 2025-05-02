da0035f5a7808d3510928877ce6bee403dd2c8a6
rkspx <kosiriki@gmail.com>
2025-05-02 10:56:59 +0700

    fix h3 nested under h3

--- src/components/Common/ProductItem.tsx
+++ src/components/Common/ProductItem.tsx
@@ -134,7 +134,7 @@ const ProductItem = ({ item }: { item: Product; }) => {
                 className="font-medium text-dark ease-out duration-200 hover:text-blue mb-1.5"
                 onClick={() => handleProductDetails()}
             >
-                <ProductTitle title={item.title} />
+                {item.title}
             </h3>
 
             <span className="flex items-center gap-2 font-medium text-lg">
