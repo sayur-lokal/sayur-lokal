702c38a1b7b65d7bd5dd3eddfb93c228d6c3a376
rkspx <kosiriki@gmail.com>
2025-05-02 11:02:21 +0700

    fix incorrect image review path

--- src/components/Common/CartSidebarModal/SingleItem.tsx
+++ src/components/Common/CartSidebarModal/SingleItem.tsx
@@ -15,7 +15,7 @@ const SingleItem = ({ item, removeItemFromCart }) => {
     <div className="flex items-center justify-between gap-5">
       <div className="w-full flex items-center gap-6">
         <div className="flex items-center justify-center rounded-[10px] bg-gray-3 max-w-[90px] w-full h-22.5">
-          <Image src={item.imgs?.thumbnails[0]} alt="product" width={100} height={100} />
+          {item.imgs && item.imgs.thumbnails ? <Image src={item.imgs?.thumbnails[0]}  className="aspect-square object-contain" alt="product" width={100} height={100} /> : null}
         </div>
 
         <div>
