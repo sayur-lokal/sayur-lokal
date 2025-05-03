9cf9341e14d63c7efe8b064155665f9866abed79
rkspx <kosiriki@gmail.com>
2025-05-03 13:29:39 +0700

    set seller-admin to use subpages instead of single page

--- src/app/(site)/(pages)/seller-admin/page.tsx
+++ src/app/(site)/(pages)/seller-admin/page.tsx
@@ -1,18 +1,6 @@
-import Breadcrumb from "@/components/Common/Breadcrumb";
+"use client"
 import Dashboard from "./dashboard";
 
-const SellerAdminPage = () => {
-    return (
-        <main>
-            <Breadcrumb title={"Seller Dashboard"} pages={["seller dashboard"]} />
-
-            <section className="overflow-hidden relative pb-20 lg:pt-10 xl:pt-14">
-                <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0">
-                    <Dashboard />
-                </div>
-            </section>
-        </main>
-    );
-};
-
-export default SellerAdminPage;
+export default function Page() {
+    return (<Dashboard />)
+}
