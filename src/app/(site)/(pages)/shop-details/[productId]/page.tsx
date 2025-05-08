61176bbae4f0b874f17b45c1bbb40718df50ae5b
rkspx <kosiriki@gmail.com>
2025-05-08 11:30:09 +0700

    fix validation error causing shop-detail doesn't work

--- src/app/(site)/(pages)/shop-details/[productId]/page.tsx
+++ src/app/(site)/(pages)/shop-details/[productId]/page.tsx
@@ -2,28 +2,22 @@ import React from "react";
 import ShopDetails from "@/components/ShopDetails";
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
   title: "Shop Details Page | sayurlokal",
   description: "This is Shop Details Page for sayurlokal",
   // other metadata
 };
 
+interface Props {
+  params: {
+    productId: string;
+  };
+}
 
-// interface Props {
-//   params: {
-//     productId: string; 
-//   };
-// };
-
-export default function ShopDetailsPage() {
-  // const { productId } = await Promise.resolve(params);
-
+export default function ShopDetailsPage({ params }: Props) {
   return (
     <main>
       <ShopDetails />
     </main>
   );
-};
-
-// export default ShopDetailsPage;
+}
