c8d2dad7db1ff1d2f234b67b2cb03605f89eb4da
rkspx <kosiriki@gmail.com>
2025-05-10 10:18:29 +0700

    limit latest products to only 12 products

--- src/components/Home/NewArrivals/index.tsx
+++ src/components/Home/NewArrivals/index.tsx
@@ -48,7 +48,7 @@ const NewArrival = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-7.5 gap-y-9">
           {/* <!-- New Arrivals item --> */}
-          {shopData.map((item, key) => (
+          {shopData.slice(0, 12).map((item, key) => (
             <ProductItem item={item} key={key} />
           ))}
         </div>
