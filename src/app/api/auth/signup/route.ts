9e331298252235d381f3c36988c5cbf476ef62a8
rkspx <kosiriki@gmail.com>
2025-05-10 10:16:53 +0700

    add hash step to mocked signup step

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,45 +1,103 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { createUser } from "@/lib/mock-users";
+import {
+  ACCESS_TOKEN_EXPIRY,
+  generateAccessToken,
+  generateRefreshToken,
+  REFRSH_TOKEN_EXPIRY,
+} from "@/lib/jwt";
 
 const signupSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, "Password must contain at least one uppercase letter, one lowercase letter, and one number"),       
-    role: z.enum(["buyer", "seller"]),
-})
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8)
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    ),
+  role: z.enum(["buyer", "seller"]),
+});
 
-export async function POST(request: NextRequest):Promise<NextResponse> {
-    try {
-        const body = await request.formData()
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  try {
+    const body = await request.formData();
     const parsed = signupSchema.safeParse({
-        email: body.get("email"),
-        password: body.get("password"),
-        role: body.get("role"),
-    })
+      email: body.get("email"),
+      password: body.get("password"),
+      role: body.get("role"),
+    });
     if (!parsed.success) {
-        return NextResponse.json({ error: parsed.error.format() }, { status: 400 })
+      return NextResponse.json(
+        { error: parsed.error.format() },
+        { status: 400 }
+      );
     }
 
-    const { email, password, role } = parsed.data
-    // TODO: hash the password and store it in the database
+    const { email, password, role } = parsed.data;
 
-    const uid = uuidv4()
+    const user = await createUser({
+      name: email,
+      email,
+      password,
+      role,
+    });
 
-    if (role === "buyer") {
-        return NextResponse.redirect(new URL("/", request.url))
-    } else {
-        return NextResponse.redirect(new URL(`/signup/shop?uid=${uid}`, request.url))
-    }
-    } catch (e) {
-        const uid = uuidv4()
-        console.warn({
-            error_id: uid,
-            message: e instanceof Error ? e.message : `${e}`,
-            detail: e,
-        })
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      roles: user.role,
+      iat: Math.floor(Date.now() / 1000),
+    };
 
-        return NextResponse.redirect(new URL(`/signup/seller?error=ERR_INTERNAL_SERVER_ERROR&error_id=${uid}`, request.url))
-    }
+    const [accessToken, refreshToken] = await Promise.all([
+      generateAccessToken(payload),
+      generateRefreshToken(payload),
+    ]);
+    
+    
+    const response =
+      role == "buyer"
+        ? NextResponse.redirect(new URL("/", request.url))
+        : NextResponse.redirect(
+            new URL(`/signup/shop?uid=${user.id}`, request.url)
+          );
+    response.cookies.set({
+      name: "accessToken",
+      value: accessToken,
+      httpOnly: true,
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: parseInt(ACCESS_TOKEN_EXPIRY) * 60,
+    });
 
+    response.cookies.set({
+      name: "refreshToken",
+      value: refreshToken,
+      httpOnly: true,
+      path: "/api/auth/refresh",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: parseInt(REFRSH_TOKEN_EXPIRY) * 60 * 60 * 24,
+    });
 
+    return response;
+  } catch (e) {
+    const uid = uuidv4();
+    console.warn({
+      error_id: uid,
+      message: e instanceof Error ? e.message : `${e}`,
+      detail: e,
+    });
+
+    return NextResponse.redirect(
+      new URL(
+        `/signup/seller?error=ERR_INTERNAL_SERVER_ERROR&error_id=${uid}`,
+        request.url
+      )
+    );
+  }
 }
